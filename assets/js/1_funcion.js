147d7fd2d20d3dc88ed2f314da3ad3a06691b8ab
beto <alberto.luna87@hotmail.com>
2024-06-15 21:59:26 +1200

    correccion ejercicio

--- assets/js/1_funcion.js
+++ assets/js/1_funcion.js
@@ -1,30 +1,11 @@
-let verificar = document.querySelector("#verificar")
+// Funcion de declaración
 
-verificar.addEventListener('click',()=>{
-    let a = document.querySelector("#a").value
-    let b = document.querySelector("#b").value
-    let c = document.querySelector("#c").value
-
-a = parseInt(a)
-b = parseInt(b)
-c = parseInt(c)
-
-let suma = a + b + c
-
-
-    document.querySelector("h3").innerHTML = "Resultado " + suma 
+function example(a, b, c){    
+ return a+b+c;
+  }
     
-        
-return a+b+c
-})
+// Funcion de expresión
 
-
-
-//const suma = (a, b, c)=>{
-    //return a+b+c
-
-
-
-// function example(a, b, c){
- //   return a+b+c
- //   } 
+example = function(a, b ,c){
+ return a+b+c;
+}
